6112921e8d8244de6b3e9f2360dd3f2de8a885b1
AngelEsteva <luis.lopez.esteva@gmail.com>
2026-05-02 16:50:01 -0600

    primer commit

---
+++ src/com/angelesteva/estructurasecuencial/Ejercicio_1.java
@@ -0,0 +1,9 @@
+package com.angelesteva.estructurasecuencial;
+/*
+Dados dos números enteros, se requiere calcular la suma de ambos.
+ */
+public class Ejercicio_1 {
+    public static void main() {
+
+    }
+}
