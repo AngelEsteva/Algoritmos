d55803fd54367f6bf418a8853318f7534f014114
AngelEsteva <luis.lopez.esteva@gmail.com>
2026-05-02 18:12:38 -0600

    Ejercicio 1

--- src/com/angelesteva/estructurasecuencial/Ejercicio_1.java
+++ src/com/angelesteva/estructurasecuencial/Ejercicio_1.java
@@ -1,9 +1,19 @@
 package com.angelesteva.estructurasecuencial;
+
+import java.util.Scanner;
+
 /*
 Dados dos números enteros, se requiere calcular la suma de ambos.
  */
 public class Ejercicio_1 {
-    public static void main() {
-
+    static void main() {
+    int num1,num2, suma;
+    Scanner sc = new Scanner(System.in);
+    num1 = sc.nextInt();
+    num2 = sc.nextInt();
+    suma = num1 + num2;
+    IO.println("Total"+suma);
+    System.out.println("Total"+suma);
+    sc.close();
     }
 }
